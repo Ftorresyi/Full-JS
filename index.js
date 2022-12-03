3922ccf975ed2fee0d4d297fb6a7a84f3e8156da
Yindi Torres <yindiyft@gmail.com>
2022-12-03 14:33:16 -0300

    Refatorando Callbacks para Promises

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 1- Obter o numero de telefone de um usuário a partir de seu ID
 2- Obter o endereço do usuario pelo ID. 
 */
+// importamos um módulo interno do Node.JS que faz a manipulação de Promises
 
 // o usuário retornará um objeto Promisse, que possui uma função que retorna um Callback (diz oq será feito quando resolver - valor, e quando houver erro - reject)
-function obterusuario(callback){
+function obterusuario(usuario){
     return new Promise (function resolvePromise(resolve, reject){
         setTimeout( function(){
+            //return reject(new Error('DEU RUIM DE VERDADE!'))
             return resolve ({
                 id: 1,
                 nome: 'Yindi',
@@ -18,13 +20,16 @@ function obterusuario(callback){
     
 }
 
-function obterTelefone(idUsuario, callback ){
-    setTimeout( () => {
-        return callback(null, {
-            telefone: '979162664',
-            ddd:'21'
-        })
-    }, 2000);
+function obterTelefone(idUsuario){
+    return new Promise(function resolvePromise(resolve, reject){
+        setTimeout( () => {
+            return resolve({
+                telefone: '979162664',
+                ddd:'21'
+            })
+        }, 2000);
+    })
+    
 }
 
 function obterEndereco(idUsuario, callback){
@@ -43,6 +48,18 @@ const usuarioPromise = obterusuario()
 
 //recebe o callback apenas com o resultado
 usuarioPromise
+    .then(function(usuario){
+        return obterTelefone(usuario.id)
+        .then(function resolverTelefone(result){
+            return {
+                usuario: {
+                    nome: usuario.nome,
+                    id: usuario.id
+                },
+                telefone: result
+            }
+        })
+    })
     .then(function(resultado){
         console.log('resultado', resultado)
     })
