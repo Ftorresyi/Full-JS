507b7c16bdc7c558678885e644fc5dde3793f343
Yindi Torres <yindiyft@gmail.com>
2022-12-04 23:45:32 -0300

    Promisify

--- Promisses.js
+++ Promisses.js
@@ -1,15 +1,15 @@
-//Será implementado SYNC e AWAIT invés de PRIMISES
-//São funções que iniciaram no C# e depois foram implementadas no JS
-//Deve ser usado apenas quando necessário para evitar deixar o código lento ou pesado.
-
-
-//Removemos as implementações das promises do código anterior e deixamos apenas as assinaturas das funções
+/*
+0- Obter um usuário
+1- Obter o numero de telefone de um usuário a partir de seu ID
+2- Obter o endereço do usuario pelo ID. 
+*/
+// importamos um módulo interno do Node.JS que faz a manipulação de Promises
 //Ele é basicamente usado para converter um método que retorna respostas usando uma função de retorno
 const util = require('util')
 const obterEnderecoAsync = util.promisify(obterEndereco)
 
 // o usuário retornará um objeto Promisse, que possui uma função que retorna um Callback (diz oq será feito quando resolver - valor, e quando houver erro - reject)
-function obterUsuario(usuario){
+function obterusuario(usuario){
     return new Promise (function resolvePromise(resolve, reject){
         setTimeout( function(){
             //return reject(new Error('DEU RUIM DE VERDADE!'))
@@ -44,20 +44,50 @@ function obterEndereco(idUsuario, callback){
     }, 2000);
 }
 
-//1° Passo: Adicionar a palavra async -> aumaticamente ela retornará um Promise
-// A forma de manipular erro será via Try e Catch
-async function main (){
-    try{
-        const usuario = await obterUsuario()
-        const telefone = await obterTelefone(usuario.id)
-        const endereco = await obterEnderecoAsync(usuario.id)
-    }
-    catch (error){
-        console.error('DEU RUIM', error)
-    }
-}
+// para manipular o sucesso usamos a função .then()
+// para manipular erros, usamos o .catch()
+//usuario -> telefone -> telefone
+const usuarioPromise = obterusuario()
 
-/* PROMOISEFY:
+//recebe o callback apenas com o resultado
+usuarioPromise
+    .then(function(usuario){
+        return obterTelefone(usuario.id)
+        .then(function resolverTelefone(result){
+            return {
+                usuario: {
+                    nome: usuario.nome,
+                    id: usuario.id
+                },
+                telefone: result
+            }
+        })
+    })
+    //esse .then recebe resultado do .then anterior. Fará isso para obter endereço da promise do NodeJS (obterEnderecoAsync)
+    .then(function(resultado){
+        const endereco = obterEnderecoAsync(resultado.usuario.id)
+        //return endereco;
+        return endereco.then(function resolverEndereco(result){
+            return{
+                usuario: resultado.usuario,
+                telefone: resultado.telefone,
+                endereco: result
+            }
+        })
+    })
+    .then(function(resultado){
+        console.log(`
+            Nome: ${resultado.usuario.nome}
+            Endereco: ${resultado.endereco.rua}, ${resultado.endereco.numero}
+            Telefone: (${resultado.telefone.ddd}) ${resultado.telefone.telefone}
+        `)
+    })
+    //Se a função acima der erro cairá no cairá no .catch abaixo
+    .catch(function(error){
+        console.error('DEU RUIM', error)
+    })
+    
+    /* PROMISEFY:
 O métodoutil.promisify()define no módulo utilitários da biblioteca padrão Node.js. 
 Ele é basicamente usado para converter um método que retorna respostas usando uma função de retorno 
 de chamada para retornar respostas em um objeto promise. Normalmente, muito em breve, torna-se muito 
