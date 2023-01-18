b8feb5a1e96a4668beb12245e890ddedb14c41b3
Yindi Torres <yindiyft@gmail.com>
2023-01-18 16:57:49 -0300

    Leitura e Teste com Manipulaçao de Arquivos

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -8,21 +8,23 @@ const {
 
 const readFileAsync = promisify(readFile) //Para trabalhar com Promisses dentro do arquivo JS
 
-const path = require('path')
+//const path = require('path')
 
 class Database{
     constructor(){
-        this.NOME_ARQUIVO = 'herois.json'
+        this.NOME_ARQUIVO = './herois.json'
     }
     async obterDadosArquivo(){
         const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
-        return JSON.parse(arquivo.toString())
-
+        return JSON.parse(arquivo.toString()) //converte o arquivo para string e para JSON
     }
     escreverArquivo(){
 
     }
-    listar(){
+    async listar(id){
+        const dados = await this.obterDadosArquivo()
+        const dadosFiltrados = dados.filter(item=> (item ? (item.id===id): true)) // ? indica que se o id for diferente de null, undefined ou zero vai ser o usado o item.id
+        //O ':true' informa que se não for para nenhum id, a função irá trazer a lista completa, porém se passar id, será lista o id específico
         return null
     }
 }
--- 06-cli/test.js
+++ 06-cli/test.js
@@ -2,18 +2,22 @@ const {
     deepEqual, ok
 } = require('assert');
 
+const database = require('./database');
+
 const DEFAULT_ITEM_CADASTRAR = {
     nome: 'Flash',
     poder:'Speed',
     id:1
 }
 
+//Aqui vamos descreve o teste de código que ira compara as informações do banco de dados (herois.json) com os dados cadastrados na cons DEFAULT_ITEM_CADASTRAR
+
 describe ('Suite de manipulacao de Herois', () => { //() é uma função anonima e => é aerofunction
 
     it (  'deve inicializar o objeto Herois (PESQUISAR), usando arquivos', async() => {
         const expected = DEFAULT_ITEM_CADASTRAR
-        //
-        ok(null, expected)
+        const [resultado] = await database.listar(expected.id)
+        deepEqual(resultado, expected)
         
     })
 })
