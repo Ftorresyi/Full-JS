3f828265de4832fe41cf29525d594ca126373a12
Yindi Torres <yindiyft@gmail.com>
2023-04-16 16:11:42 -0300

    Facilitando o uso do Status Code com a lib http-status-code

--- AutenticadorREST/src/index.ts
+++ AutenticadorREST/src/index.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response, NextFunction } from "express";
 import usersRoute from "./routes/users.route";
+import {StatusCodes} from 'http-status-codes';
 
 const app = express(); // cria uma instancia da lib express
 
@@ -9,7 +10,7 @@ app.use(usersRoute);
 //CONFIGURAR TODAS AS ROTAS DO PROJETO:
 //cria um endpoint com um callback para descobrir o estatus da aplicação
 app.get('/status', (req: Request, res: Response, next: NextFunction)=>{
-     res.status(200).send({foo: 'Sucesso Total! Você é incrível!'});
+     res.status(StatusCodes.OK).send({foo: 'Sucesso Total! Você é incrível!'});
 });
 
 //Fica escutando uma porta HTTP para comunicaçao. O get trabalha junto com o listen
--- AutenticadorREST/src/routes/users.route.ts
+++ AutenticadorREST/src/routes/users.route.ts
@@ -15,6 +15,18 @@ usersRoute.get('/users', (req: Request, res: Response, next: NextFunction)=>{
     const users = [{username: 'Yindi'}];
     res.status(200).send(users);
 });
+// os dois pontos antes da variavel uuid indica que valor do parâmetro capturado dentro da url pode ser dinâmico
+//req - captura informacao da requisicao,
+//res - responde as informaçoes da requisiçao
+//req e res trabalham em conjunto 
+usersRoute.get('/users/:uuid', (req: Request<{uuid:string}>, res: Response, next: NextFunction)=>{
+    const uuid = req.params.uuid//captura paramentro da url
+    //req.query //captura caracteres string na url no get
+    //req.header //captura o cabeçalho
+    //bancoDeDados.getUserByUUid(uuid);
+    //200, 400, 404, etc são status code (o que aconteceu) da aplicacao que está dentro da padronizaçao REST
+    res.status(200).send({uuid}); //envia uma responta com o status 200 (OK), e envia na tela o mesmo uuid capturado na url
+});
 
 // A app deve ser exportada, pois está fora do arquivo principal. Depois ela deve ser importada na app desejada
 export default usersRoute;
