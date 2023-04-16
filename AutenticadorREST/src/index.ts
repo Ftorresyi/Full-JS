bf3b3586234f6bcd5755ef9750d382f060a914c1
Yindi Torres <yindiyft@gmail.com>
2023-04-16 13:55:08 -0300

    Adicionando o typescript com express ts-node-dev

---
+++ AutenticadorREST/src/index.ts
@@ -0,0 +1,17 @@
+import express, {Request, Response, NextFunction } from "express";
+
+const app = express(); // cria uma instancia da lib express
+
+console.log("Executei!");
+
+
+//CONFIGURAR TODAS AS ROTAS DO PROJETO:
+//cria um endpoint com um callback para descobrir o estatus da aplicação
+app.get('/status', (req: Request, res: Response, next: NextFunction)=>{
+     res.status(200).send({foo: 'Sucesso Total! Você é incrível!'});
+});
+
+//Fica escutando uma porta HTTP para comunicaçao. O get trabalha junto com o listen
+app.listen(3000, ()=>{
+    console.log('Aplicaçao executando na porta 3000');
+});
